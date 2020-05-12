6edf25ee9bf109daeb83465353a4081388b4d11d
austint33 <austintumlinson@gmail.com>
2020-05-12 18:21:45 -0500

    fixed arrow link for view and adjusted position

--- src/Components/MainView/MainView.js
+++ src/Components/MainView/MainView.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Back from '../../Pictures/back-arrow.png';
 import './MainView.css';
 
 function MainView(props) {
     return (
         <div className="main-view-container">
+            <Link className="back2" to="/peppers-sweet"><img className="back-arrow2" src={Back} alt="back"/></Link>
             <h2 className="main-view-title">Bell Pepper</h2>
             <img alt="bell-pepper" className="large-image" src="https://i.imgur.com/RVRPnwKl.jpg"/>
             <div className="main-view-description">Naturally grown sweet Bell Peppers, yields vibrant large fruits, seeds and pods are not color specific, the colors range from red, yellow, green, orange, and some even purple! Seeds are carefully dried before storage and sealed in ziplock bags.</div>
