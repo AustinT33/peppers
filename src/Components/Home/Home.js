d03c30bee684fb8e37244e4c73e5fcc4bcd4db91
austint33 <austintumlinson@gmail.com>
2020-05-12 19:56:08 -0500

    fixed home bio

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,7 +7,7 @@ function Home() {
             <div className="home-container">
                 <div className='title-container'>
                     <h1 className="title">Welcome to Green Wolf Forest</h1>
-                    <div className="bio">I'm a small garden hobbyist crazy about peppers, but I have an issue, I can't handle the heat so I have tons of hot peppers to share! I grow peppers of all kinds, all plants and produce are completely organic and naturally grown. If you're crazy about peppers like myself, scroll on down!</div>
+                    <div className="bio">I'm a small garden hobbyist crazy about peppers, but I have an issue, I can't handle the heat so I have tons of hot peppers to share! I grow peppers of all kinds, all plants and produce are soil or hydroponically grown. If you're crazy about peppers like myself, scroll on down!</div>
                 </div>
                 <div className="opts-container">
                     <h2 className="sub-title">Explore Peppers</h2>
